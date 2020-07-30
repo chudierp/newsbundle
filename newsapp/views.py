50c002a78437ceff78c809c552ac46069e3eba35
Chudier <chudierp@gmail.com>
2020-07-30 10:53:08 -0700

    added heroku app

--- newsapp/views.py
+++ newsapp/views.py
@@ -4,6 +4,7 @@ from newsapi import NewsApiClient
 # Create your views here.
 
 def index(request):
+
     newsapi = NewsApiClient(api_key='93ec2510842647889c3913445b754c83')
     # /v2/top-headlines
     top_headlines = newsapi.get_top_headlines(sources='bbc-news,the-verge')
@@ -16,14 +17,16 @@ def index(request):
     desc = []
     news = []
     img = []
+    url = []
 
     for i in range(len(all_articles)):
         my_articles = all_articles[i]
 
         news.append(my_articles['title'])
         desc.append(my_articles['description'])
+        url.append(my_articles['url'])
         img.append(my_articles['urlToImage'])
 
-    mylist = zip(news, desc, img)
+    mylist = zip(news, desc, url, img)
 
     return render(request, 'index.html', context={'mylist':mylist})
